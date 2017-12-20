39f2f794059e98ec18456e504ec9e0858588a05c
Chris Jezior <chrisjezior@Chriss-MacBook-Pro.local>
2017-12-20 18:19:13 -0500

    First commit

---
+++ assets/javascript/appquestions.js
@@ -0,0 +1,78 @@
+var questions = [{
+  "question": "Who won the nomination of the Democratic Party to run for president in 2016?",
+  "option1": "Al Gore",
+  "option2": "Bernie Sanders",
+  "option3": "Hillary Clinton",
+  "option4": "Bill Clinton",
+  "answer": "3"
+}, {
+  "question": "Who was the vice president that served under President Barack Obama?",
+  "option1": "Evam Bayh",
+  "option2": "Joe Biden",
+  "option3": "Tim Kaine",
+  "option4": "Kathleen Sebelius",
+  "answer": "2"
+}, {
+  "question": "What late American singer was the mayor of Palm Springs California from 1988 to 1992?",
+  "option1": "Jerry Butler",
+  "option2": "Martha Reeves",
+  "option3": "Jon Fishman",
+  "option4": "Sonny Bono",
+  "answer": "4"
+}, {
+  "question": "Who was vice president of the United States when Lincoln was assassinated?",
+  "option1": "Andrew Johnson",
+  "option2": "Hannibal Hamlin",
+  "option3": "James S. Sherman",
+  "option4": "Millard Fillmore",
+  "answer": "1",
+}, {
+  "question": "Who is the oldest person to be elected to the office of President of the United States?",
+  "option1": "Ronald Reagan",
+  "option2": "William H. Harrison",
+  "option3": "Donald Trump",
+  "option4": "James Buchanan",
+  "answer": "3"
+}, {
+  "question": "The oldest parliament in the world belongs to what country?",
+  "option1": "Iceland",
+  "option2": "United Kingdom",
+  "option3": "Denmark",
+  "option4": "Japan",
+  "answer": "1"
+}, {
+  "question": "Barack Obama was first elected president of the United States in what year?",
+  "option1": "2012",
+  "option2": "2008",
+  "option3": "2009",
+  "option4": "2007",
+  "answer": "2"
+}, {
+  "question": "How many U.S. presidents were only children?",
+  "option1": "3",
+  "option2": "14",
+  "option3": "20",
+  "option4": "None",
+  "answer": "4"
+}, {
+  "question": "Who was the first Roman Catholic to be Vice President of the United States of America?",
+  "option1": "Al Gore",
+  "option2": "Dan Quayle",
+  "option3": "Joe Biden",
+  "option4": "Dick Cheney",
+  "answer": "3"
+}, {
+  "question": "Who was the first First Lady to be elected to public office?",
+  "option1": "Bird Johnson",
+  "option2": "Edith Wilson",
+  "option3": "Hillary Rodham Clinton",
+  "option4": "Jacqueline Kennedy Onassis",
+  "answer": "3"
+}, {
+  "question": "How many US Supreme Court justices are there?",
+  "option1": "3",
+  "option2": "9",
+  "option3": "8",
+  "option4": "12",
+  "answer": "2"
+}];
